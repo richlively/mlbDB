056c1438a11975236f4858334f47b57aa6f9445c
user <user@192.168.183.130>
2017-04-12 22:54:34 -0400

    updated with callable statements and more setting team data

--- MLB/src/conversion/Convert.java
+++ MLB/src/conversion/Convert.java
@@ -1,5 +1,6 @@
 package conversion;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -15,6 +16,8 @@ import bo.FieldingStats;
 import bo.PitchingStats;
 import bo.Player;
 import bo.PlayerSeason;
+import bo.Team;
+import bo.TeamSeason;
 import dataaccesslayer.HibernateUtil;
 
 public class Convert {
@@ -172,11 +175,14 @@ public class Convert {
 						// for debugging comment previous line, uncomment next line
 						"from Team where (yearID = 1871 and lgID = 'NA' and teamID = 'BS1') or ( yearID = 1871 and lgID = 'NA' and teamID = 'CH1') ;" );
 			ResultSet rs = ps.executeQuery();
+			
 			int count=0; // for progress feedback only
 			while (rs.next()) {
+				CallableStatement young_stmt = conn.prepareCall("{call youngest_year(?)}");
+				CallableStatement oldest_stmt = conn.prepareCall("{call oldest_year(?)}");
 				count++;
 				// this just gives us some progress feedback
-				if (count % 500 == 0) System.out.println("num players: " + count);
+				if (count % 500 == 0) System.out.println("num teams: " + count);
 				String tid = rs.getString("teamID");
 				String name = rs.getString("name");
 				// this check is for data scrubbing
@@ -184,34 +190,22 @@ public class Convert {
 				if (tid == null	|| tid.isEmpty() || 
 					name == null || name.isEmpty() ) continue;
 				// this far //
-				// TODO: Build Team class with appropriate get / set methods
 				
-				Player p = new Player();
-				p.setName(firstName + " " + lastName);
-				p.setGivenName(rs.getString("nameGiven"));
-				java.util.Date birthDay = convertIntsToDate(rs.getInt("birthYear"), rs.getInt("birthMonth"), rs.getInt("birthDay"));
-				if (birthDay!=null) p.setBirthDay(birthDay);
-				java.util.Date deathDay = convertIntsToDate(rs.getInt("deathYear"), rs.getInt("deathMonth"), rs.getInt("deathDay"));
-				if (deathDay!=null) p.setDeathDay(deathDay);
-				// need to do some data scrubbing for bats and throws columns
-				String hand = rs.getString("bats");
-				if (hand!=null && hand.equalsIgnoreCase("B")) {
-					hand = "S";
-				} 
-				p.setBattingHand(hand);
-				hand = rs.getString("throws");
-				p.setThrowingHand(hand);
-				p.setBirthCity(rs.getString("birthCity"));
-				p.setBirthState(rs.getString("birthState"));
-				java.util.Date firstGame = rs.getDate("debut");
-				if (firstGame!=null) p.setFirstGame(firstGame);
-				java.util.Date lastGame = rs.getDate("finalGame");
-				if (lastGame!=null) p.setLastGame(lastGame);
-				addPositions(p, pid);
-				// players bio collected, now go after stats
-				addSeasons(p, pid);
-				// we can now persist player, and the seasons and stats will cascade
-				HibernateUtil.persistPlayer(p);
+				young_stmt.setString(1, tid);
+				oldest_stmt.setString(1, tid);
+				
+				Team t = new Team();
+				t.setName(name);
+				t.setLeague(rs.getString("lgID"));
+				// todo see how to get top result from statement set, how to set date with just yera
+				t.setYearFounded(convertIntsToDate(young_stmt.getInt(1), 1, 1));
+				t.setYearLast(convertIntsToDate(oldest_stmt.getInt(1),1,1));
+				
+//				addTeamSeason(p, pid);
+//				// players bio collected, now go after stats
+//				addSeasons(p, pid);
+//				// we can now persist player, and the seasons and stats will cascade
+//				HibernateUtil.persistPlayer(p);
 			}
 			rs.close();
 			ps.close();
