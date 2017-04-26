774fa0cbd16a76791bbaf4a72467bff93752eea1
Nathan Shinabarger <wretchedturnedrighteous@gmail.com>
2017-04-25 23:24:00 -0400

    Began update to TeamController

--- MLB/src/controller/TeamController.java
+++ MLB/src/controller/TeamController.java
@@ -12,20 +12,123 @@ import dataaccesslayer.HibernateUtil;
 
 public class TeamController extends BaseController {
 
-	@Override
-	public void init(String query) {
-		// TODO Auto-generated method stub
-		System.out.println("building dynamic html for team");
-		view = new TeamView();
-		process(query);
-	}
-	
-	@Override
-	protected void performAction() {
-		// TODO Auto-generated method stub
+    @Override
+    public void init(String query) {
+            // TODO Auto-generated method stub
+            System.out.println("building dynamic html for team");
+            view = new TeamView();
+            process(query);
+    }
 
-	}
+    @Override
+    protected void performAction() {
+        String action = keyVals.get("action");
+        System.out.println("playercontroller performing action: " + action);
+        if (action.equalsIgnoreCase(ACT_SEARCHFORM)) {
+            processSearchForm();
+        } else if (action.equalsIgnoreCase(ACT_SEARCH)) {
+            processSearch();
+        } else if (action.equalsIgnoreCase(ACT_DETAIL)) {
+            processDetails();
+        } 
+    }
 
-	
+    protected void processSearchForm() {
+        view.buildSearchForm();
+    }
+    
+    protected final void processSearch() {
+        String name = keyVals.get("name");
+        if (name == null) {
+            return;
+        }
+        String v = keyVals.get("exact");
+        boolean exact = (v != null && v.equalsIgnoreCase("on"));
+        List<Team> bos = HibernateUtil.retrieveTeamsByName(name, exact);
+        view.printSearchResultsMessage(name, exact);
+        buildSearchResultsTableTeam(bos);
+        view.buildLinkToSearch();
+    }
 
+    protected final void processDetails() {
+        String id = keyVals.get("id");
+        if (id == null) {
+            return;
+        }
+        Team t = (Team) HibernateUtil.retrieveTeamById(Integer.valueOf(id));
+        if (t == null) return;
+        buildSearchResultsTableTeamDetail(t);
+        view.buildLinkToSearch();
+    }
+
+    private void buildSearchResultsTableTeam(List<Team> bos) {
+        // need a row for the table headers
+        String[][] table = new String[bos.size() + 1][10];
+        table[0][0] = "Id";
+        table[0][1] = "Name";
+        table[0][2] = "Lifetime Salary";
+        table[0][3] = "Games Played";
+        table[0][4] = "First Game";
+        table[0][5] = "Last Game";
+        table[0][6] = "Career Home Runs";
+        table[0][7] = "Career Hits";
+        table[0][8] = "Career Batting Average";
+        table[0][9] = "Career Steals";
+        for (int i = 0; i < bos.size(); i++) {
+            Team t = bos.get(i);
+            TeamSeasonStats stats = new TeamSeasonStats(t);
+            String tid = t.getId().toString();
+            table[i + 1][0] = view.encodeLink(new String[]{"id"}, new String[]{tid}, tid, ACT_DETAIL, SSP_PLAYER);
+            table[i + 1][1] = t.getName();
+            table[i + 1][2] = DOLLAR_FORMAT.format(stats.getSalary());
+            table[i + 1][3] = stats.getGamesPlayed().toString();
+            table[i + 1][4] = formatDate(t.getFirstGame());
+            table[i + 1][5] = formatDate(t.getLastGame());
+            table[i + 1][6] = stats.getHomeRuns().toString();
+            table[i + 1][7] = stats.getHits().toString();
+            table[i + 1][8] = DOUBLE_FORMAT.format(stats.getBattingAverage());
+            table[i + 1][9] = stats.getSteals().toString();
+        }
+        view.buildTable(table);
+    }
+    
+    private void buildSearchResultsTableTeamDetail(Team t) {
+    	Set<TeamSeason> seasons = t.getSeasons();
+    	List<TeamSeason> list = new ArrayList<TeamSeason>(seasons);
+    	Collections.sort(list, TeamSeason.teamSeasonsComparator);
+    	// build 2 tables.  first the player details, then the season details
+        // need a row for the table headers
+        String[][] teamTable = new String[2][6];
+        teamTable[0][0] = "Name";
+        teamTable[0][1] = "League";
+        teamTable[0][2] = "Year Founded";
+        teamTable[0][3] = "Last Year";
+        teamTable[1][0] = t.getName();
+        teamTable[1][1] = t.getLeague();
+        teamTable[1][2] = t.getYearFounded().toString();
+        teamTable[1][3] = t.getYearLast().toString();
+        
+        view.buildTable(teamTable);
+        // now for seasons
+        String[][] seasonTable = new String[seasons.size()+1][7];
+        seasonTable[0][0] = "Year";
+        seasonTable[0][1] = "Games Played";
+        seasonTable[0][2] = "Wins";
+        seasonTable[0][3] = "Losses";
+        seasonTable[0][4] = "At Bats";
+        seasonTable[0][5] = "Batting Average";
+        seasonTable[0][6] = "Home Runs";
+        int i = 0;
+        for (TeamSeason ts: list) {
+        	i++;
+        	seasonTable[i][0] = ts.getYear().toString();
+        	seasonTable[i][1] = ts.getGamesPlayed().toString();
+        	seasonTable[i][2] = DOLLAR_FORMAT.format(ts.getSalary());
+        	seasonTable[i][3] = ts.getBattingStats().getHits().toString();
+        	seasonTable[i][4] = ts.getBattingStats().getAtBats().toString();
+        	seasonTable[i][5] = DOUBLE_FORMAT.format(ts.getBattingAverage());
+        	seasonTable[i][6] = ts.getBattingStats().getHomeRuns().toString();
+        }
+        view.buildTable(seasonTable);
+    }
 }
